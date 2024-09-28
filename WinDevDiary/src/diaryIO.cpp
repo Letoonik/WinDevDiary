9857ea64d665a926be44a7d6f4bc01ab30bef1d5
Letoonik <Letoonik@github and other platforms>
2024-09-28 10:23:33 +0200

    Added working encryption option (decrytion comming soon) 2

--- WinDevDiary/src/diaryIO.cpp
+++ WinDevDiary/src/diaryIO.cpp
@@ -8,123 +8,124 @@ using json = nlohmann::json;
 
 extern bool isEncrypted;
 std::string password = "Hase12774#3??23";
+const char* pwd = password.data();
 
 namespace WDDsave 
 {
-
 	void encryptEntry(std::string textData) 
 	{
 		// INIT DATA
 		json data;
-		std::ofstream jsonFile;
-
+		std::ofstream encFile;
+		encFile.open("example.enc", std::ofstream::binary | std::ofstream::app);
 		data["diaryEntryText"] = textData;
 
-		jsonFile.open("example.enc");
-
-		unsigned char pwHash[crypto_box_SEEDBYTES];
-		unsigned char nonce[crypto_secretbox_NONCEBYTES];
+		unsigned char key[crypto_box_SEEDBYTES];
 		unsigned char salt[crypto_pwhash_SALTBYTES];
+		unsigned char header[crypto_secretstream_xchacha20poly1305_HEADERBYTES];
 
-		randombytes_buf(salt, sizeof salt);
+		randombytes_buf(salt, sizeof(salt));
+
+
+		if(std::filesystem::exists("example.enc"))
+		{
+			encFile.close();
+			if (remove("example.enc"))
+			{
+				int g = 0;
+			}
+			else 
+			{
+				encFile.open("example.enc", std::ofstream::binary | std::ofstream::app);
+			}
+		}
 
 		bool tempTrue = true;
-
 		if (tempTrue) {
-			if (crypto_pwhash(pwHash, sizeof(pwHash), password.data(), sizeof(password), salt, crypto_pwhash_OPSLIMIT_INTERACTIVE, crypto_pwhash_MEMLIMIT_INTERACTIVE,
-				crypto_pwhash_ALG_DEFAULT) != 0)
+			if (crypto_pwhash(key, sizeof(key), pwd, strlen(pwd), salt, crypto_pwhash_OPSLIMIT_INTERACTIVE, crypto_pwhash_MEMLIMIT_INTERACTIVE, crypto_pwhash_ALG_DEFAULT) != 0)
 				int sdf = 0; //Placeholder, fix later
 			else
 			{
-				unsigned char header[crypto_secretstream_xchacha20poly1305_HEADERBYTES];
 
 				crypto_secretstream_xchacha20poly1305_state state;
+				crypto_secretstream_xchacha20poly1305_init_push(&state, header, key);
 
-				crypto_secretstream_xchacha20poly1305_keygen(pwHash);
-				crypto_secretstream_xchacha20poly1305_init_push(&state, header, pwHash);
-
-				//PROBLEMSTELLE
+				encFile.write((const char*)header, crypto_secretstream_xchacha20poly1305_HEADERBYTES);
+				encFile.write((const char*)salt, crypto_pwhash_SALTBYTES);
 
 				std::string dataString = data.dump();
 
-				unsigned char* trap = new unsigned char[dataString.size()];
-				std::copy(dataString.begin(), dataString.end(), trap);
-				//PROBLEMSTELLE ENDE
+				//size_t abced = dataString.size();
+				//const char* def2 = (const char*)dataString.size();
+				//encFile.write((const char*)def2, 8);
 
+				unsigned char* entryData = reinterpret_cast<unsigned char*>(&dataString[0]);
 
-				unsigned char trap2[1000] = "sfdkkkkkkkkkkkkkkkkkkkkkskldjfklsjöldkfjaklsjdklfjaskldjkfljöalsjdklfjsaeifasbxobcvoibiosofihioehoisdfkljsdklfjlöakshdfoiabsodivboiypxciovhosdihfiosehiorhoiahsifoahoshdpfhasdpf";
-				unsigned char crypted[sizeof(trap2)] = "";
+				unsigned char * encryptedMessage = new unsigned char[dataString.size() + crypto_secretstream_xchacha20poly1305_ABYTES];
 
+				unsigned long long textLen = strlen((char*)entryData);
 
-				crypto_secretstream_xchacha20poly1305_push(&state, crypted, NULL, trap2, sizeof(trap2) + crypto_secretstream_xchacha20poly1305_ABYTES, NULL, 0, crypto_secretstream_xchacha20poly1305_TAG_FINAL);
+				crypto_secretstream_xchacha20poly1305_push(&state, encryptedMessage, NULL, entryData, textLen + crypto_secretstream_xchacha20poly1305_ABYTES, NULL, 0, crypto_secretstream_xchacha20poly1305_TAG_FINAL);
 
-				if (jsonFile.is_open())
+				if (encFile.is_open())
 				{
-					jsonFile << crypted;
+					encFile.write((const char*)encryptedMessage, dataString.size() + crypto_secretstream_xchacha20poly1305_ABYTES);
 				}
 				else
 				{
-					int askldfjklasdf = 0; // Also just for testing
+					assert(false);
 				}
-
-				jsonFile.close();
+				encFile.close();
 
 			}
 		}
 		else
 		{
-			int a = 0; //Placeholder, fix later
+			assert(false);
 		}
 	}
 
 	std::string decryptEntry() 
 	{
 		json data;
-		std::ifstream jsonFile;
+		std::ifstream encFile;
 		std::string vec;
-		unsigned char nonce[crypto_secretbox_NONCEBYTES];
-		unsigned char salt[crypto_pwhash_SALTBYTES];
 
-		randombytes_buf(salt, sizeof salt);
-
-		jsonFile.open("example.enc");
-
-		if (jsonFile.is_open())
-		{
-			int asdfksdf = 3;
-		}
-		else
-		{
-			return "Failed to load/open JSON file";
-		}
+		encFile.open("example.enc", std::ofstream::binary);
 
 		unsigned char tag = 0;
-		std::string cyph;
-		jsonFile >> cyph;
-		unsigned char* c1 = new unsigned char[cyph.size()];
+		unsigned char key[crypto_secretstream_xchacha20poly1305_KEYBYTES];
+		unsigned char header[crypto_secretstream_xchacha20poly1305_HEADERBYTES];
+		encFile.read((char *)header, crypto_secretstream_xchacha20poly1305_HEADERBYTES);
+		unsigned char salt[crypto_pwhash_SALTBYTES];
+		encFile.read((char*)salt, crypto_pwhash_SALTBYTES);
 
 		crypto_secretstream_xchacha20poly1305_state state;
-		unsigned char pwHash[crypto_secretstream_xchacha20poly1305_KEYBYTES];
-		unsigned char header[crypto_secretstream_xchacha20poly1305_HEADERBYTES];
-
-		crypto_pwhash(pwHash, sizeof(pwHash), password.data(), sizeof(password), salt, crypto_pwhash_OPSLIMIT_INTERACTIVE, crypto_pwhash_MEMLIMIT_INTERACTIVE,
-			crypto_pwhash_ALG_DEFAULT);
 
 
-		if (crypto_secretstream_xchacha20poly1305_init_pull(&state, header, pwHash) != 0)
+		crypto_pwhash(key, sizeof(key), pwd, strlen(pwd), salt, crypto_pwhash_OPSLIMIT_INTERACTIVE, crypto_pwhash_MEMLIMIT_INTERACTIVE, crypto_pwhash_ALG_DEFAULT);
+		
+		encFile.seekg(0, encFile.end);
+		int length = encFile.tellg();
+		length -= 40;
+		encFile.seekg(40);
+		int current = encFile.tellg();
+
+		unsigned char * c1 = new unsigned char[length - 30]; // TODO: is the encrypted lenght = unencrypted lenght?
+		encFile.read((char *) c1, 80);
+
+		if (crypto_secretstream_xchacha20poly1305_init_pull(&state, header, key) != 0)
 		{
 			int asdfjk = 0;
 		}
 
-		//assert(tag == 0);
+		assert(tag == 0);
 
 		unsigned char m1[10000];
-
-		if (crypto_secretstream_xchacha20poly1305_pull(&state, m1, NULL, &tag, c1, 106, NULL, 0) != 0)
-		{
-			int asdjklsfjklsdfjkl = 0;
-		}
-		//assert(tag == 0);
+		unsigned long long m1Len = 10000;
+		//Problem Code
+		crypto_secretstream_xchacha20poly1305_pull(&state, m1, &m1Len, &tag, c1, length, NULL, 0);
+		assert(tag == 0);
 
 		data = m1;
 
